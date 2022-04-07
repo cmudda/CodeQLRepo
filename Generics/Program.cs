0f7f882ce64f6d952d4084c8eb29610c2be7c083
Chandrashekar Mudda <44369945+cmudda@users.noreply.github.com>
2022-04-07 12:56:02 +0530

    Initial changes

---
+++ Generics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            var intClass = new GetValue<int>
+            {
+                Value = 20
+            };
+
+            var stringClass = new GetValue<string>()
+            {
+                Value = "Test"
+            };
+
+            intClass.PrintValue("Integer", intClass.Value);
+            stringClass.PrintValue("String", stringClass.Value);
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class GetValue<T>
+    {
+        private T _data;
+
+        public T Value
+        {
+            get => _data;
+            set => _data = value;
+        }
+
+        public void PrintValue<TYpeOfValue>(string msg, TYpeOfValue val)
+        {
+            Console.WriteLine($"Message: {msg} and Value: {val}");
+        }
+    }
+}
